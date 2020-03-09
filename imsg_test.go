cb2aaa4ab5debe81c9f48216f343fc89525ce960
Matt Schultz <schultz@sent.com>
2020-03-09 02:20:11 -0500

    Reworked tests to be more table-driven. Removed non-fatal test error logs.

--- imsg_test.go
+++ imsg_test.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"io"
 	"reflect"
 	"testing"
 )
@@ -17,11 +18,22 @@ type imsgTest struct {
 	imsg              *IMsg
 	littleEndianBytes []byte
 	bigEndianBytes    []byte
+	expectedErrorType error
 }
 
-var imsgTests = []imsgTest{
-	{"empty imsg", &IMsg{}, []byte{0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
-	{"simple imsg", &IMsg{0xff, 0xee, 0xdd, []byte("test"), 0xcc}, []byte{0xff, 0, 0, 0, 20, 0, 0xcc, 0, 0xee, 0, 0, 0, 0xdd, 0, 0, 0, 0x74, 0x65, 0x73, 0x74}, []byte{0, 0, 0, 0xff, 0, 20, 0, 0xcc, 0, 0, 0, 0xee, 0, 0, 0, 0xdd, 0x74, 0x65, 0x73, 0x74}},
+var marshalTests = []imsgTest{
+	{"valid empty", &IMsg{}, []byte{0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, nil},
+	{"valid simple", &IMsg{0xff, 0xee, 0xdd, []byte("test"), 0xcc}, []byte{0xff, 0, 0, 0, 20, 0, 0xcc, 0, 0xee, 0, 0, 0, 0xdd, 0, 0, 0, 0x74, 0x65, 0x73, 0x74}, []byte{0, 0, 0, 0xff, 0, 20, 0, 0xcc, 0, 0, 0, 0xee, 0, 0, 0, 0xdd, 0x74, 0x65, 0x73, 0x74}, nil},
+	{"invalid data too large", &IMsg{Data: make([]byte, MaxSizeInBytes+1)}, nil, nil, &ErrDataTooLarge{}},
+}
+
+var unmarshalTests = []imsgTest{
+	{"valid empty", &IMsg{}, []byte{0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, nil},
+	{"valid simple", &IMsg{0xff, 0xee, 0xdd, []byte("test"), 0xcc}, []byte{0xff, 0, 0, 0, 20, 0, 0xcc, 0, 0xee, 0, 0, 0, 0xdd, 0, 0, 0, 0x74, 0x65, 0x73, 0x74}, []byte{0, 0, 0, 0xff, 0, 20, 0, 0xcc, 0, 0, 0, 0xee, 0, 0, 0, 0xdd, 0x74, 0x65, 0x73, 0x74}, nil},
+	{"invalid < min length", nil, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, &ErrLengthOutOfBounds{}},
+	{"invalid > max length", nil, []byte{0, 0, 0, 0, 0xff, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0xff, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, &ErrLengthOutOfBounds{}},
+	{"invalid insufficient data", nil, []byte{0, 0, 0, 0, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, []byte{0, 0, 0, 0, 0, 0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, &ErrInsufficientData{}},
+	{"invalid insufficient data", nil, []byte{0, 0, 0}, []byte{0, 0, 0}, io.ErrUnexpectedEOF},
 }
 
 func TestComposeIMsg(t *testing.T) {
@@ -43,6 +55,75 @@ func TestComposeIMsg(t *testing.T) {
 	}
 }
 
+func TestReadIMsg(t *testing.T) {
+	// Store out the determined system endianness before manually manipulating it
+	systemEndianness := endianness
+
+	var (
+		tt   imsgTest
+		buf  *bytes.Reader
+		imsg *IMsg
+		err  error
+	)
+
+	// First run tests for little endian systems
+	endianness = binary.LittleEndian
+	for _, tt = range unmarshalTests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf = bytes.NewReader(tt.littleEndianBytes)
+			imsg, err = ReadIMsg(buf)
+
+			if tt.expectedErrorType == nil {
+				if err != nil {
+					t.Fatalf("unexpected ReadIMsg failure: %s", err)
+				}
+
+				if !reflect.DeepEqual(imsg, tt.imsg) {
+					t.Fatalf("result of ReadIMsg does not match expected output (%#v != %#v)", imsg, tt.imsg)
+				}
+			} else {
+				if err == nil {
+					t.Fatalf("incorrectly read imsg")
+				}
+
+				if !errors.As(err, &tt.expectedErrorType) {
+					t.Fatalf("failed to read imsg in unexpected way: %s", err)
+				}
+			}
+		})
+	}
+
+	// Next run tests for big endian systems
+	endianness = binary.BigEndian
+	for _, tt = range unmarshalTests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf = bytes.NewReader(tt.bigEndianBytes)
+			imsg, err = ReadIMsg(buf)
+
+			if tt.expectedErrorType == nil {
+				if err != nil {
+					t.Fatalf("unexpected ReadIMsg failure: %s", err)
+				}
+
+				if !reflect.DeepEqual(imsg, tt.imsg) {
+					t.Fatalf("result of ReadIMsg does not match expected output (%#v != %#v)", imsg, tt.imsg)
+				}
+			} else {
+				if err == nil {
+					t.Fatalf("incorrectly read imsg")
+				}
+
+				if !errors.As(err, &tt.expectedErrorType) {
+					t.Fatalf("failed to read imsg in unexpected way: %s", err)
+				}
+			}
+		})
+	}
+
+	// Restore the determined system endianness
+	endianness = systemEndianness
+}
+
 func TestMarshalBinary(t *testing.T) {
 	// Store out the determined system endianness before manually manipulating it
 	systemEndianness := endianness
@@ -51,161 +132,58 @@ func TestMarshalBinary(t *testing.T) {
 		tt     imsgTest
 		result []byte
 		err    error
-		edtl   *ErrDataTooLarge
 	)
 
 	// First run tests for little endian systems
 	endianness = binary.LittleEndian
-	for _, tt = range imsgTests {
+	for _, tt = range marshalTests {
 		t.Run(tt.name, func(t *testing.T) {
 			result, err = tt.imsg.MarshalBinary()
-			if err != nil {
-				t.Error(err)
-			}
 
-			if !bytes.Equal(result, tt.littleEndianBytes) {
-				t.Fatalf("little endian result (% x) does not match expected output (% x)", result, tt.littleEndianBytes)
+			if tt.expectedErrorType == nil {
+				if err != nil {
+					t.Fatalf("unexpected MarshalBinary failure: %s", err)
+				}
+
+				if !bytes.Equal(result, tt.littleEndianBytes) {
+					t.Fatalf("result of MarshalBinary does not match expected output (% x != % x)", result, tt.littleEndianBytes)
+				}
+			} else {
+				if err == nil {
+					t.Fatal("incorrectly marshalled imsg to binary")
+				}
+
+				if !errors.As(err, &tt.expectedErrorType) {
+					t.Fatalf("failed to marshal imsg to binary in unexpected way: %s", err)
+				}
 			}
 		})
 	}
 
 	// Next run tests for big endian systems
 	endianness = binary.BigEndian
-	for _, tt = range imsgTests {
+	for _, tt = range marshalTests {
 		t.Run(tt.name, func(t *testing.T) {
 			result, err = tt.imsg.MarshalBinary()
-			if err != nil {
-				t.Error(err)
-			}
 
-			if !bytes.Equal(result, tt.bigEndianBytes) {
-				t.Fatalf("big endian result (% x) does not match expected output (% x)", result, tt.bigEndianBytes)
-			}
-		})
-	}
+			if tt.expectedErrorType == nil {
+				if err != nil {
+					t.Fatalf("unexpected MarshalBinary failure: %s", err)
+				}
 
-	// Ensure imsgs that are too large can't be marshalled
-	imsg := &IMsg{
-		Data: make([]byte, MaxSizeInBytes),
-	}
-	_, err = imsg.MarshalBinary()
-	if err == nil {
-		t.Fatalf("incorrectly marshalled an imsg with oversized ancillary data")
-	}
-	if !errors.As(err, &edtl) {
-		t.Fatalf("failed to marshal an imsg in an unexpected way: %s", err)
-	}
+				if !bytes.Equal(result, tt.bigEndianBytes) {
+					t.Fatalf("result of MarshalBinary does not match expected output (% x != % x)", result, tt.bigEndianBytes)
+				}
+			} else {
+				if err == nil {
+					t.Fatal("incorrectly marshalled imsg to binary")
+				}
 
-	// Restore the determined system endianness
-	endianness = systemEndianness
-}
-
-func TestReadIMsg(t *testing.T) {
-	// Store out the determined system endianness before manually manipulating it
-	systemEndianness := endianness
-
-	var (
-		tt    imsgTest
-		buf   *bytes.Reader
-		imsg  *IMsg
-		err   error
-		eloob *ErrLengthOutOfBounds
-		eid   *ErrInsufficientData
-	)
-
-	// First run tests for little endian systems
-	endianness = binary.LittleEndian
-	for _, tt = range imsgTests {
-		t.Run(tt.name, func(t *testing.T) {
-			buf = bytes.NewReader(tt.littleEndianBytes)
-			imsg, err = ReadIMsg(buf)
-			if err != nil {
-				t.Error(err)
-			}
-
-			if !reflect.DeepEqual(imsg, tt.imsg) {
-				t.Fatalf("little endian result (% x) does not match expected output (% x)", imsg, tt.imsg)
+				if !errors.As(err, &tt.expectedErrorType) {
+					t.Fatalf("failed to marshal imsg to binary in unexpected way: %s", err)
+				}
 			}
 		})
-	}
-
-	// Next run tests for big endian systems
-	endianness = binary.BigEndian
-	for _, tt = range imsgTests {
-		t.Run(tt.name, func(t *testing.T) {
-			buf = bytes.NewReader(tt.bigEndianBytes)
-			imsg, err = ReadIMsg(buf)
-			if err != nil {
-				t.Error(err)
-			}
-
-			if !reflect.DeepEqual(imsg, tt.imsg) {
-				t.Fatalf("big endian result (% x) does not match expected output (% x)", imsg, tt.imsg)
-			}
-		})
-	}
-
-	// Ensure imsgs that have an invalid length aren't unmarshalled
-	buf = bytes.NewReader(
-		[]byte{
-			0, 0, 0, 0, // type
-			0, 0, // length < header size
-			0, 0, // flags
-			0, 0, 0, 0, // peer id
-			0, 0, 0, 0, // pid
-		},
-	)
-	_, err = ReadIMsg(buf)
-	if err == nil {
-		t.Fatalf("incorrectly read an imsg with invalidly small length")
-	}
-	if !errors.As(err, &eloob) {
-		t.Fatalf("failed to read an imsg in an unexpected way: %s", err)
-	}
-
-	buf = bytes.NewReader(
-		[]byte{
-			0, 0, 0, 0, // type
-			0xff, 0xff, // length > maximum size
-			0, 0, // flags
-			0, 0, 0, 0, // peer id
-			0, 0, 0, 0, // pid
-		},
-	)
-	_, err = ReadIMsg(buf)
-	if err == nil {
-		t.Fatalf("incorrectly read an imsg with invalidly large length")
-	}
-	if !errors.As(err, &eloob) {
-		t.Fatalf("failed to read an imsg in an unexpected way: %s", err)
-	}
-
-	buf = bytes.NewReader(
-		[]byte{
-			0, 0, 0, 0,
-			0, 0xff,
-			0, 0,
-			0, 0, 0, 0,
-			0, 0, 0, 0,
-
-			1, 2, 3, 4,
-		},
-	)
-	_, err = ReadIMsg(buf)
-	if err == nil {
-		t.Fatalf("incorrectly read an imsg with invalidly short ancillary data")
-	}
-	if !errors.As(err, &eid) {
-		t.Fatalf("failed to read an imsg in an unexpected way: %s", err)
-	}
-
-	// Ensure messages smaller than the header size don't get unmershalled
-	buf = bytes.NewReader(
-		[]byte{0, 0, 0}, // smaller than the uint32 that describes the Type field
-	)
-	_, err = ReadIMsg(buf)
-	if err == nil {
-		t.Fatalf("incorrectly read a malformed imsg")
 	}
 
 	// Restore the determined system endianness
