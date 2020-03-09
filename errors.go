d496de3c4344bcf2019c46168a259c83ca32a693
Matt Schultz <schultz@sent.com>
2020-03-09 00:25:51 -0500

    Add error type for message length out of bounds

--- errors.go
+++ errors.go
@@ -5,15 +5,33 @@ import "fmt"
 // ErrDataTooLarge is returned when the provided ancillary data is larger than
 // is allowed.
 type ErrDataTooLarge struct {
-	DataLength    int
-	MaximumLength uint16
+	DataLength int
+	MaxLength  uint16
 }
 
-// Error implements the error interface
+// Error implements the error interface.
 func (e *ErrDataTooLarge) Error() string {
 	return fmt.Sprintf(
 		"imsg: provided data is too large (%d bytes > %d bytes)",
 		e.DataLength,
-		e.MaximumLength,
+		e.MaxLength,
+	)
+}
+
+// ErrLengthOutOfBounds is returned when the length parameter is either smaller
+// than the imsg header size or larger than the allowed maximum size.
+type ErrLengthOutOfBounds struct {
+	Length    uint16
+	MinLength uint16
+	MaxLength uint16
+}
+
+// Error implements the error interface.
+func (e *ErrLengthOutOfBounds) Error() string {
+	return fmt.Sprintf(
+		"imsg: message length (%d bytes) is out of allowed bounds (%d - %d bytes)",
+		e.Length,
+		e.MinLength,
+		e.MaxLength,
 	)
 }
